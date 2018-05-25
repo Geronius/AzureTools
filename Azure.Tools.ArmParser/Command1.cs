2f1cd2f96fc98f74aa058b0b4bd7dcf962c93cf6
Gert Vloo <gertvloo@hotmail.com>
2018-05-25 07:56:41 +0200

    Eerste versie van ArmParser

---
+++ Azure.Tools.ArmParser/Command1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Linq;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Azure.Tools.ArmParser
+{
+    internal sealed class Command1
+    {
+        public const int CommandId = 0x0100;
+
+        public static readonly Guid CommandSet = new Guid("b394839a-d886-44d2-94c9-ffeeb48d97d5");
+
+        private readonly Package package;
+
+        private Command1(Package package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException("package");
+            }
+
+
+            if (ServiceProvider.GetService(typeof(IMenuCommandService)) is OleMenuCommandService commandService)
+            {
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.MenuItemCallback, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        public static Command1 Instance
+        {
+            get;
+            private set;
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return package;
+            }
+        }
+
+        public static void Initialize(Package package)
+        {
+            Instance = new Command1(package);
+        }
+
+        private void MenuItemCallback(object sender, EventArgs e)
+        {
+            var dte = (EnvDTE.DTE)this.ServiceProvider.GetService(typeof(EnvDTE.DTE));
+            var selectedItems = dte.SelectedItems;
+
+            if (selectedItems != null)
+            {
+                foreach (EnvDTE.SelectedItem selectedItem in selectedItems)
+                {
+                    var projectItem = selectedItem.ProjectItem as EnvDTE.ProjectItem;
+
+                    if (projectItem != null)
+                    {
+                        try
+                        {
+                            ParseArmSource.Execute(projectItem.FileNames[0]);
+                        }
+                        catch (Exception ex)
+                        {
+                            var message = $"{projectItem.Name} has Exception: {ex.Message}";
+
+
+                            // Show a message box to prove we were here
+                            VsShellUtilities.ShowMessageBox(
+                                this.ServiceProvider,
+                                message,
+                                "Error",
+                                OLEMSGICON.OLEMSGICON_WARNING,
+                                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Azure.Tools.ArmParser/ToolWindow1Package.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
+
+namespace Azure.Tools.ArmParser
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideToolWindow(typeof(ToolWindow1))]
+    [Guid(ToolWindow1Package.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class ToolWindow1Package : AsyncPackage
+    {
+        /// <summary>
+        /// ToolWindow1Package GUID string.
+        /// </summary>
+        public const string PackageGuidString = "5c3b3d1b-e05b-42bd-b5cc-be92273f0cf3";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolWindow1"/> class.
+        /// </summary>
+        public ToolWindow1Package()
+        {
+            base.Initialize();
+            Command1.Initialize(this);
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await ToolWindow1Command.InitializeAsync(this);
+        }
+
+        #endregion
+    }
+}
